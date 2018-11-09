3275e798a4b5fb3c26ecd055ea249e14bf359343
Arthur.mf <arthurmf.amf@gmail.com>
2018-11-09 09:53:47 +0100

    Git/bubble sort
    Step 1 Assignment 2. Repositary creation and implentation algorithm bubble sort.

---
+++ assignment.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  9 09:44:02 2018
+
+@author: arthurmaroquenefroissart
+"""
+
+# Bubble sort implementation.
+#
+# This function returns nothing because it works by mutating the lst.
+def bubble(lst):
+    for j in range(0, len(lst) - 1):
+        for i in range(0, len(lst) - j - 1):
+            if lst[i] > lst[i + 1]:
+                temp = lst[i]
+                lst[i] = lst[i + 1]
+                lst[i + 1] = temp
+                
+#%%
+                
+def merge(left, right):
+    result = []
+
+    while len(left) != 0 and len(right) != 0:
+        if left[0] < right[0]:
+            result.append(left[0])
+            left.pop(0)
+        else:
+            result.append(right[0])
+            right.pop(0)
+            
+    if len(left) == 0:
+        result += right
+    else:
+        result += left
+            
+    return result
+
+#%%
